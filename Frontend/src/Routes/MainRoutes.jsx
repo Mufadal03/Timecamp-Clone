1bcaf82404a26160f8d4b3dda68915b93f442342
Niranjan Kumar <kniranajn2903@gmail.com>
2022-09-29 19:34:37 +0530

    day 2 task done

--- Frontend/src/Routes/MainRoutes.jsx
+++ Frontend/src/Routes/MainRoutes.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
-
+import Feature  from "../Pages/Features"
 const MainRoutes = () => {
   return (
       <Routes>
           <Route path='/' element={<h1>Home</h1>} />
+          <Route path='/features' element={<Feature />} />
    </Routes>
   )
 }
