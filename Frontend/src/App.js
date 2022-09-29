c90d15b3a544bd5365cb56a7dc29c4e9720e6157
sandip suryawanshi <suryawanshisandip299@gmail.com>
2022-09-30 00:01:11 +0530

    login and signup

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import Login from './Components/Login';
 import MainRoutes from './Routes/MainRoutes';
 
 function App() {
   return (
     <div className="App">
       <MainRoutes />
+      
     </div>
   );
 }
--- Frontend/src/Routes/MainRoutes.jsx
+++ Frontend/src/Routes/MainRoutes.jsx
@@ -1,12 +1,16 @@
-import React from 'react'
-import { Route, Routes } from 'react-router-dom'
-
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Login from "../Components/Login";
+import Mainnav from "../Components/Mainnav";
+import Signup from "../Components/Signup";
 const MainRoutes = () => {
   return (
-      <Routes>
-          <Route path='/' element={<h1>Home</h1>} />
-   </Routes>
-  )
-}
+    <Routes>
+      <Route path="/" element={<Mainnav />} />
+      <Route path="/signup" element={<Signup />} />
+      <Route path="/login" element={<Login />} />
+    </Routes>
+  );
+};
 
-export default MainRoutes
+export default MainRoutes;
