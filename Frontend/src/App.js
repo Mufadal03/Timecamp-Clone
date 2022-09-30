d64ac88cb5b0e95bce36a71b892193f504551e7b
sandip suryawanshi <suryawanshisandip299@gmail.com>
2022-10-01 01:35:06 +0530

    project page

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,10 +1,15 @@
-import './App.css';
-import MainRoutes from './Routes/MainRoutes';
+import "./App.css";
+import Addprojects from "./Pages/Project/Addprojects";
+import MainRoutes from "./Routes/MainRoutes";
+import ProjectcontextProvider from "./Context/ProjectContext";
 
 function App() {
   return (
     <div className="App">
       <MainRoutes />
+      <ProjectcontextProvider>
+        <Addprojects />
+      </ProjectcontextProvider>
     </div>
   );
 }
---
+++ Frontend/src/Pages/Project/Addprojects.jsx
@@ -0,0 +1,180 @@
+import {
+  Box,
+  Button,
+  Input,
+  Popover,
+  PopoverBody,
+  Text,
+  PopoverContent,
+  PopoverTrigger,
+} from "@chakra-ui/react";
+import React, { useState } from "react";
+import { AiOutlineArrowsAlt } from "react-icons/ai";
+import { HiChevronDown } from "react-icons/hi";
+import { FiFilter } from "react-icons/fi";
+import { BsThreeDots } from "react-icons/bs";
+import Addtasks from "./Addtasks";
+import Edittasks from "./Edittasks";
+import { useContext } from "react";
+import { ProjectContext } from "../../Context/ProjectContext";
+
+
+const Addprojects = () => {
+
+  const [edit,setEdit]=useState(true)
+  const projectcontext=useContext(ProjectContext)
+    console.log(projectcontext);
+  return (
+    
+      <Box height="55vh" marginLeft={"3%"} width={"70%"} overflow={"scroll"}>
+        <Box height={"24"} p="6" width="70%" zIndex={-1}>
+          <Box display={"flex"} gap="15px" alignItems={"center"}>
+            <Input
+              focusBorderColor="#25cf60"
+              border={"0.2px solid gray"}
+              placeholder="Search"
+              width="230px"
+            />
+
+            <Button
+              /*onClick={handleCreateProject} */ color={"aliceblue"}
+              bgColor="#25cf60"
+              _hover={{ opacity: "0.7" }}
+            >
+              Add project
+            </Button>
+
+            <Popover placement="bottom-start" isLazy>
+              <PopoverTrigger>
+                <Box display={"flex"}>
+                  <FiFilter />
+                  <Text>Filter</Text>
+                  <HiChevronDown />
+                </Box>
+              </PopoverTrigger>
+              <PopoverContent width="200px">
+                <PopoverBody p="2">
+                  <Text
+                    fontWeight={"semibold"}
+                    color={"blackAlpha.900"}
+                    _hover={{
+                      bgColor: "#25cf60",
+                      cursor: "pointer",
+                      borderRadius: "5px",
+                      color: "aliceblue",
+                    }}
+                    p="2"
+                  >
+                    All tasks
+                  </Text>
+                  <Box
+                    _hover={{
+                      bgColor: "#25cf60",
+                      cursor: "pointer",
+                      borderRadius: "5px",
+                      color: "aliceblue",
+                    }}
+                    p="2"
+                  >
+                    <Text
+                      _hover={{ color: "aliceblue" }}
+                      fontWeight={"semibold"}
+                      color={"blackAlpha.900"}
+                    >
+                      My tasks
+                    </Text>
+                  </Box>
+                </PopoverBody>
+              </PopoverContent>
+            </Popover>
+
+            <Button
+              variant={"outline"}
+              fontWeight="normal"
+              bgColor={"#fff"}
+              color="gray.600"
+            >
+              <AiOutlineArrowsAlt size={"20px"} />
+            </Button>
+            <Button
+              variant={"outline"}
+              fontWeight="normal"
+              bgColor={"#fff"}
+              color="gray.600"
+            >
+              Archived
+            </Button>
+
+            <Popover placement="bottom-start" isLazy>
+              <PopoverTrigger>
+                <Button
+                  variant={"outline"}
+                  fontWeight="normal"
+                  bgColor={"#fff"}
+                  color="gray.600"
+                >
+                  <BsThreeDots />
+                </Button>
+              </PopoverTrigger>
+              <PopoverContent width="200px">
+                <PopoverBody p="2">
+                  <Text
+                    fontWeight={"semibold"}
+                    color={"blackAlpha.900"}
+                    _hover={{
+                      bgColor: "#25cf60",
+                      cursor: "pointer",
+                      borderRadius: "5px",
+                      color: "aliceblue",
+                    }}
+                    p="2"
+                  >
+                    Import From CSV
+                  </Text>
+                  <Box
+                    _hover={{
+                      bgColor: "#25cf60",
+                      cursor: "pointer",
+                      borderRadius: "5px",
+                      color: "aliceblue",
+                    }}
+                    p="2"
+                  >
+                    <Text
+                      _hover={{ color: "aliceblue" }}
+                      fontWeight={"semibold"}
+                      color={"blackAlpha.900"}
+                    >
+                      Import From add-ons
+                    </Text>
+                    <Text fontSize={"10px"} color="gray.600">
+                      Trello,Basecamp,Asana,Insightly,jira,podio and more...
+                    </Text>
+                  </Box>
+                </PopoverBody>
+              </PopoverContent>
+            </Popover>
+          </Box>
+        </Box>
+        <Box display={"flex"}>
+          <Addtasks />
+          {edit ? (
+            <Edittasks />
+          ) : (
+            <Box
+              padding={"30px"}
+              width={"40%"}
+              marginLeft="1%"
+              border="1px solid gray"
+              borderRadius="9px"
+            >
+              Choose project or task to edit
+            </Box>
+          )}
+        </Box>
+      </Box>
+   
+  );
+};
+
+export default Addprojects;
