cf3ecfa963a8bbaf808fb98e676e7d7e9b4e9175
Mufadal Sadriwala <sadrimufadal@gmail.com>
2022-09-30 10:17:46 +0530

    Update MainRoutes.jsx

--- Frontend/src/Routes/MainRoutes.jsx
+++ Frontend/src/Routes/MainRoutes.jsx
@@ -1,20 +1,13 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
-
+import Homepage from '../Pages/Home/Homepage'
 import Feature  from "../Pages/Features"
 const MainRoutes = () => {
   return (
       <Routes>
-          <Route path='/' element={<h1>Home</h1>} />
+         <Route path='/' element={<Homepage />} /> 
           <Route path='/features' element={<Feature />} />
-import Homepage from '../Pages/Home/Homepage'
 
-const MainRoutes = () => {
-  return (
       <Routes>
-          <Route path='/' element={<Homepage />} />
-   </Routes>
-  )
-}
-
+)}
 export default MainRoutes
