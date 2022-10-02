a65019741cf755840acc8100955b18acfd53d292
sandip suryawanshi <suryawanshisandip299@gmail.com>
2022-10-02 09:50:01 +0530

    day4

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,18 +1,11 @@
 import "./App.css";
-import Addprojects from "./Pages/Project/Addprojects";
 import MainRoutes from "./Routes/MainRoutes";
-import ProjectcontextProvider from "./Context/ProjectContext";
-import './App.css';
-import Login from './Components/Login';
-
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <MainRoutes />
-      <ProjectcontextProvider>
-        <Addprojects />
-      </ProjectcontextProvider>
     </div>
   );
 }
--- Frontend/src/Routes/MainRoutes.jsx
+++ Frontend/src/Routes/MainRoutes.jsx
@@ -1,24 +1,33 @@
-import React from 'react';
+import React from "react";
 import Login from "../Components/Login";
 import Mainnav from "../Components/Mainnav";
 import Signup from "../Components/Signup";
-import { Route, Routes } from 'react-router-dom';
-import Homepage from '../Pages/Home/Homepage';
-import Feature  from "../Pages/Features";
-import Integration from '../Components/Integration/Integration'
-import { Price } from '../Pages/Pricing/Price';
-
+import { Route, Routes } from "react-router-dom";
+import Homepage from "../Pages/Home/Homepage";
+import Feature from "../Pages/Features";
+import Integration from "../Components/Integration/Integration";
+import { Price } from "../Pages/Pricing/Price";
+import ProjectcontextProvider from "../Context/ProjectContext";
+import Addprojects from "../Pages/Project/Addprojects";
 const MainRoutes = () => {
   return (
-      <Routes>
-         <Route path='/' element={<Homepage />} /> 
-         
+    <Routes>
+      <Route path="/" element={<Homepage />} />
+
       <Route path="/signup" element={<Signup />} />
       <Route path="/login" element={<Login />} />
-          <Route path='/features' element={<Feature />} />
-           <Route path='/integration' element={<Integration />} />
-           <Route path='/price' element={<Price/>} />
-          
-      </Routes>
-)}
-export default MainRoutes
+      <Route path="/features" element={<Feature />} />
+      <Route path="/integration" element={<Integration />} />
+      <Route path="/price" element={<Price />} />
+      <Route
+        path="/projects"
+        element={
+          <ProjectcontextProvider>
+            <Addprojects />
+          </ProjectcontextProvider>
+        }
+      />
+    </Routes>
+  );
+};
+export default MainRoutes;
