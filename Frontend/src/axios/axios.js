482c7f77cc248f95146ab5b4ac639769e971a68b
Mufadal03 <sadrimufadal@gmail.com>
2022-12-04 21:37:43 +0530

    deployment

--- Frontend/src/axios/axios.js
+++ Frontend/src/axios/axios.js
@@ -3,5 +3,5 @@ import axios from "axios"
 
 export default axios.create({
     // baseURL:"http://localhost:4000"
-    baseURL:"https://timescamp.herokuapp.com/"
+    baseURL:"https://time-camp-backend.vercel.app/"
 })
